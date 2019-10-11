dabcdcdf89a5d306553e663c69d10194063fb5cf
tmcdos <tmcdos@abv.bg>
2019-10-11 16:04:02 +0300

    MetaData did not work for some reason so I moved the version information in the Description config field for the registrar module

--- modules/registrars/namesrs/namesrs.php
+++ modules/registrars/namesrs/namesrs.php
@@ -12,17 +12,10 @@ use WHMCS\Database\Capsule as Capsule;
 $pdo = Capsule::connection()->getPdo();
 define('API_HOST',"api.domainname.systems");
 
-function namesrs_MetaData()
-{
-  return array(
-    'DisplayName' => 'NameSRS - v'.VERSION.' ('.STAMP.')',
-  );
-}
-
 function namesrs_getConfigArray()
 {
 	$configarray = array(
-	  //"Description" => array("Type" => "System", "Value" => "Version ".VERSION),
+	  "Description" => array("Type" => "System", "Value" => "version ".VERSION.' ('.STAMP.')'),
 	  "API_key" => array( "Type" => "text", "Size" => "65", "Description" => "Enter your API key here", "FriendlyName" => "API key" ),
 	  "Base_URL" => array( "Type" => "text", "Size" => "25", "Default" => API_HOST, "Description" => "Hostname for API endpoints", "FriendlyName" => "Base URL"),
 	  "AutoExpire" => array( "Type" => "yesno", "Size" => "20", "Description" => "Do not use NameISP's auto-renew feature. Let WHMCS handle the renew","FriendlyName" =>"Auto Expire"),
--- modules/registrars/namesrs/version.php
+++ modules/registrars/namesrs/version.php
@@ -1,1 +1,1 @@
-<?php define('VERSION',22); define('STAMP','2019-10-11, 16:00'); 
+<?php define('VERSION',23); define('STAMP','2019-10-11, 16:04'); 
